3cf238cbd1df14838c4bb5328cfc911b77edbf94
ansuvin <dkstnqls0925@naver.com>
2021-01-10 23:39:49 +0900

    [UPDATE] Random Number use Random

--- Pra01_lottoNumber/app/src/main/java/com/example/pra01_lottonumber/MainActivity.kt
+++ Pra01_lottoNumber/app/src/main/java/com/example/pra01_lottonumber/MainActivity.kt
@@ -3,9 +3,8 @@ package com.example.pra01_lottonumber
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import android.widget.Button
 import androidx.cardview.widget.CardView
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,7 +16,12 @@ class MainActivity : AppCompatActivity() {
         val nameCard = findViewById<CardView>(R.id.nameCard)
 
         randomCard.setOnClickListener {
-            startActivity(Intent(this, ResultActivity::class.java))
+            val intent = Intent(this, ResultActivity::class.java)
+
+            // intent의 결과 데이터를 전달 Int리스트를 전달 -> "putIntegerArrayListExtra"사용
+            intent.putIntegerArrayListExtra("result", ArrayList(getRandomLottoNumbeers()))
+
+            startActivity(intent)
         }
 
         constellationCard.setOnClickListener {
@@ -28,5 +32,26 @@ class MainActivity : AppCompatActivity() {
             startActivity(Intent(this, NameActivity::class.java))
         }
 
+    }
+
+    // 램덤으로 1~45 번호중 하나의 번호를 생성하는 함수
+    fun getRandomLottoNumber() : Int {     // Int 값을 반환
+        return Random().nextInt(45)+1
+    }
+
+    fun getRandomLottoNumbeers() : MutableList<Int> {
+        val lottoNumbers = mutableListOf<Int>()     // List 만들기
+
+        for(i in 1..6) {    // 6번 반복
+            // 램덤 번호 임시 저장 변수
+            var number = 0
+            do{
+                number = getRandomLottoNumber()
+            }while (lottoNumbers.contains(number))      // rottonNumbers에 number 변수의 값이 없을때까지 반복
+
+            lottoNumbers.add(number)    // 중복 번호가 없으면 추가
+        }
+
+        return lottoNumbers
     }
 }
--- Pra01_lottoNumber/app/src/main/java/com/example/pra01_lottonumber/ResultActivity.kt
+++ Pra01_lottoNumber/app/src/main/java/com/example/pra01_lottonumber/ResultActivity.kt
@@ -2,10 +2,44 @@ package com.example.pra01_lottonumber
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 
 class ResultActivity : AppCompatActivity() {
+
+    val lottoImageStartId = R.drawable.ball_01
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_result)
+
+        // intent로부터 전달받은 결과 배열 가져오기
+        val result = intent.getIntegerArrayListExtra("result")
+
+        // 전달받은 결과가 있는 경우에만 실행
+        result?.let {
+            // 전달 받은 결과를 정력하여 전달한다. (sorteby)
+            updateLottoBallImage(result = result.sortedBy { it })
+        }
+    }
+
+    fun updateLottoBallImage(result: List<Int>) {   // List<Int>를 인수로 한다.
+        // 결과 사이즈가 6개 미만일 경우 에러 발생 위험으로 바로 return
+        if(result.size < 6) return
+
+        val imageview01 = findViewById<ImageView>(R.id.imageView01)
+        val imageview02 = findViewById<ImageView>(R.id.imageView02)
+        val imageview03 = findViewById<ImageView>(R.id.imageView03)
+        val imageview04 = findViewById<ImageView>(R.id.imageView04)
+        val imageview05 = findViewById<ImageView>(R.id.imageView05)
+        val imageview06 = findViewById<ImageView>(R.id.imageView06)
+
+        // ball_01 아이디에 결과값 -1 을 하면 목표하는 이미지 됨
+        imageview01.setImageResource(lottoImageStartId + (result[0]-1))
+        imageview02.setImageResource(lottoImageStartId + (result[1]-1))
+        imageview03.setImageResource(lottoImageStartId + (result[2]-1))
+        imageview04.setImageResource(lottoImageStartId + (result[3]-1))
+        imageview05.setImageResource(lottoImageStartId + (result[4]-1))
+        imageview06.setImageResource(lottoImageStartId + (result[5]-1))
+
     }
 }
