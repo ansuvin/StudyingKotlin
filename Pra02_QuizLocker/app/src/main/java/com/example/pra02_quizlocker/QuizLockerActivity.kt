5f2eea39bd9432d760aa4a7feaad535fc7bc3619
ansuvin <dkstnqls0925@naver.com>
2021-01-13 00:11:46 +0900

    [UPDATE] show quiz

--- Pra02_QuizLocker/app/src/main/java/com/example/pra02_quizlocker/QuizLockerActivity.kt
+++ Pra02_QuizLocker/app/src/main/java/com/example/pra02_quizlocker/QuizLockerActivity.kt
@@ -6,10 +6,21 @@ import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
+import android.widget.TextView
+import org.json.JSONArray
+import org.json.JSONObject
+import java.util.*
 
 class QuizLockerActivity : AppCompatActivity() {
+
+    var quiz: JSONObject? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val quizLabel = findViewById<TextView>(R.id.quizLabel)
+        val choice1 = findViewById<TextView>(R.id.choice1)
+        val choice2 = findViewById<TextView>(R.id.choice2)
 
         // 잠금화면보다 위에 위치하귀 위한 설정, 버전별로 달라서 버전별로 적용
         if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O_MR1){
@@ -27,5 +38,18 @@ class QuizLockerActivity : AppCompatActivity() {
         // 화면을 꺼진 상태로 유지
         window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
         setContentView(R.layout.activity_quiz_locker)
+
+        //퀴즈 데이터 가져오기
+        val json = assets.open("capital.json").reader().readText()
+        val quizArray = JSONArray(json)
+
+        // 퀴즈 고르기
+        quiz = quizArray.getJSONObject(Random().nextInt(quizArray.length()))
+
+        // 퀴즈 보이기
+        quizLabel.text = quiz?.getString("question")
+        choice1.text = quiz?.getString("choice1")
+        choice2.text = quiz?.getString("choice2")
+
     }
 }
