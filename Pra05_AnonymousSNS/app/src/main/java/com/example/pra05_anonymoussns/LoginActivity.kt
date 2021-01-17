f75e267d8d10255f0928797a9dc50d0d88efb3d1
ansuvin <dkstnqls0925@naver.com>
2021-01-17 19:11:49 +0900

    [UPDATE] Login,SignUp - email

--- Pra05_AnonymousSNS/app/src/main/java/com/example/pra05_anonymoussns/LoginActivity.kt
+++ Pra05_AnonymousSNS/app/src/main/java/com/example/pra05_anonymoussns/LoginActivity.kt
@@ -1,13 +1,11 @@
 package com.example.pra05_anonymoussns
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.util.Patterns
-import android.view.View
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.firebase.ui.auth.AuthUI
-import com.google.firebase.auth.ActionCodeSettings
 import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_login.*
 import java.util.regex.Pattern
@@ -34,39 +32,90 @@ class LoginActivity : AppCompatActivity() {
         setContentView(R.layout.activity_login)
 
         btn_signIn.setOnClickListener {
-            email = et_email.text.toString()
-            password = et_password.text.toString()
+            login()
+        }
 
+        btn_signUp.setOnClickListener {
+            signUp()
+        }
+    }
+
+    fun login() {
+        email = et_email.text.toString()
+        password = et_password.text.toString()
+
+        if (isValidEmail() && isValidPasswd()){
             FirebaseAuth
                 .getInstance()
                 .signInWithEmailAndPassword(email, password)
                 .addOnCompleteListener { it ->
                     if (it.isSuccessful) {
-                        Toast.makeText(applicationContext, R.string.success_login, Toast.LENGTH_SHORT).show()
+                        Toast.makeText(
+                            applicationContext,
+                            R.string.success_login,
+                            Toast.LENGTH_SHORT
+                        ).show()
                     } else {
-                        Toast.makeText(applicationContext, R.string.failed_login, Toast.LENGTH_SHORT).show()
-                        Log.e(TAG, it.exception.toString())
-                    }
-                }
-        }
-
-        btn_signUp.setOnClickListener {
-            email = et_email.text.toString()
-            password = et_password.text.toString()
-
-            FirebaseAuth
-                .getInstance()
-                .createUserWithEmailAndPassword(email, password)
-                .addOnCompleteListener { it ->
-                    if (it.isSuccessful) {
-                        Toast.makeText(applicationContext, R.string.success_signup, Toast.LENGTH_SHORT).show()
-                    } else {
-                        Toast.makeText(applicationContext, R.string.failed_signup, Toast.LENGTH_SHORT).show()
+                        Toast.makeText(
+                            applicationContext,
+                            R.string.failed_login,
+                            Toast.LENGTH_SHORT
+                        ).show()
                         Log.e(TAG, it.exception.toString())
                     }
                 }
         }
     }
 
+    fun signUp() {
+        email = et_email.text.toString()
+        password = et_password.text.toString()
+
+        if (isValidEmail() && isValidPasswd()){
+            FirebaseAuth
+                .getInstance()
+                .createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener { it ->
+                    if (it.isSuccessful) {
+                        Toast.makeText(
+                            applicationContext,
+                            R.string.success_signup,
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    } else {
+                        Toast.makeText(
+                            applicationContext,
+                            R.string.failed_signup,
+                            Toast.LENGTH_SHORT
+                        ).show()
+                        Log.e(TAG, it.exception.toString())
+                    }
+                }
+        }
+    }
+
+    // 이메일 유효성 검사
+    private fun isValidEmail(): Boolean {
+        if(email.isEmpty()){
+            Toast.makeText(applicationContext, "이메일이 비어있습니다.", Toast.LENGTH_SHORT).show()
+            return false
+        } else if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            Toast.makeText(applicationContext, "이메일 형식이 아닙니다.", Toast.LENGTH_SHORT).show()
+            return false
+        }
+        return true
+    }
+
+    // 비밀번호 유효성 검사
+    private fun isValidPasswd(): Boolean {
+        if (password.isEmpty()) {
+            Toast.makeText(applicationContext, "비밀번호가 공백입니다.", Toast.LENGTH_SHORT).show()
+            return false
+        } else if (!PASSWORD_PATTERN.matcher(password).matches()) {
+            Toast.makeText(applicationContext, "비밀번호 형식이 아닙니다.", Toast.LENGTH_SHORT).show()
+            return false
+        }
+        return true
+    }
 
 }
