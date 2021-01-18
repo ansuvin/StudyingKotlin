4c003f0b60af385f3cf5a530ae9cfa41017514f0
ansuvin <dkstnqls0925@naver.com>
2021-01-18 15:50:47 +0900

    [UPDATE] Login - Facebook

--- Pra05_AnonymousSNS/app/src/main/java/com/example/pra05_anonymoussns/FirstActivity.kt
+++ Pra05_AnonymousSNS/app/src/main/java/com/example/pra05_anonymoussns/FirstActivity.kt
@@ -1,27 +1,39 @@
 package com.example.pra05_anonymoussns
 
 import android.app.Activity
+import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageInfo
+import android.content.pm.PackageManager
+import android.content.pm.Signature
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Base64
 import android.util.Log
 import android.widget.Toast
+import com.facebook.AccessToken
+import com.facebook.CallbackManager
+import com.facebook.FacebookCallback
+import com.facebook.FacebookException
+import com.facebook.internal.Utility
+import com.facebook.login.LoginResult
 import com.google.android.gms.auth.api.Auth
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount
-import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
-import com.google.android.gms.common.api.ApiException
-import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.FacebookAuthProvider
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.auth.GoogleAuthProvider
 import kotlinx.android.synthetic.main.activity_first.*
+import java.security.MessageDigest
 
 class FirstActivity : AppCompatActivity() {
 
     val TAG = "FirstActivity"
 
+    var loginMode =""
+
+    // region GoogleLogin
     private val googleSignInIntent by lazy {
         val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
             .requestIdToken(getString(R.string.default_web_client_id)).requestEmail().build()
@@ -31,39 +43,47 @@ class FirstActivity : AppCompatActivity() {
     companion object {
         const val RESULT_CODE = 10
     }
+    //endregion
+
+    // region FaceBookLogin
+    lateinit var callbackManager : CallbackManager
+    lateinit var auth: FirebaseAuth
+    // endregion
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_first)
 
-        btnSignIn.setOnClickListener {
+        btnGoogleLogin.setOnClickListener {
+            loginMode="Google"
             startActivityForResult(googleSignInIntent, RESULT_CODE)
         }
 
-    }
-
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        if (resultCode == Activity.RESULT_OK && requestCode == RESULT_CODE) {
-            val result = Auth.GoogleSignInApi.getSignInResultFromIntent(data)
-            result?.let {
-                if (it.isSuccess) {
-                    it.signInAccount?.displayName //이름
-                    it.signInAccount?.email //이메일
-                    Log.e("Value", it.signInAccount?.email!!)
-                    // 기타 등등
-                    if (result.isSuccess) {
-                        firebaseLogin(result.signInAccount!!)
-                    } else {
-
-                    }
-                } else  {
-                    Log.e("Value", "error")
-                    // 에러 처리
-                }
+        auth = FirebaseAuth.getInstance()
+        callbackManager = CallbackManager.Factory.create()
+        btnFacebookLogin.setOnClickListener { loginMode = "FaceBook" }
+        btnFacebookLogin.setReadPermissions("email")
+        btnFacebookLogin.registerCallback(callbackManager, object : FacebookCallback<LoginResult> {
+            override fun onSuccess(result: LoginResult?) {
+                Log.e(TAG, "facebook: 성공 $result.")
+                handleFacebookAccessToken(result!!.accessToken)
             }
-        }
+
+            override fun onCancel() {
+                Log.e(TAG, "cancel")
+            }
+
+            override fun onError(error: FacebookException?) {
+                Log.e(TAG, "error $error")
+                errorText.text = error.toString()
+            }
+
+        })
+
     }
+
+    // region GoogleLogin
+
 
     private fun firebaseLogin(googleAccont: GoogleSignInAccount) {
         val credential = GoogleAuthProvider.getCredential(googleAccont.idToken, null)
@@ -77,5 +97,67 @@ class FirstActivity : AppCompatActivity() {
                     Log.e(TAG, "에러닷")
                 }
             }
+    }
+    //endregion
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        when (loginMode) {
+            "Google" -> {
+                if (resultCode == Activity.RESULT_OK && requestCode == RESULT_CODE) {
+                    val result = Auth.GoogleSignInApi.getSignInResultFromIntent(data)
+                    result?.let {
+                        if (it.isSuccess) {
+                            it.signInAccount?.displayName //이름
+                            it.signInAccount?.email //이메일
+                            Log.e("Value", it.signInAccount?.email!!)
+                            // 기타 등등
+                            if (result.isSuccess) {
+                                firebaseLogin(result.signInAccount!!)
+                            } else {
+
+                            }
+                        } else  {
+                            Log.e("Value", "error")
+                            // 에러 처리
+                        }
+                    }
+                }
+            }
+
+            "FaceBook" -> {
+                callbackManager?.onActivityResult(requestCode, resultCode, data)
+                showToast("$loginMode: onActivityResult")
+            }
+
+            else -> { }
+        }
+
+    }
+
+    private fun handleFacebookAccessToken(token: AccessToken?) {
+        Log.e(TAG, "handler: $token")
+        val credential = FacebookAuthProvider.getCredential(token!!.token)
+        auth.signInWithCredential(credential)
+            .addOnCompleteListener { it ->
+                if (it.isSuccessful){
+                    Log.e(TAG, "성공입니다~")
+                    val intent = Intent(this@FirstActivity, MainActivity::class.java)
+                    startActivity(intent)
+                } else {
+                    Log.e(TAG, "실패다 ${it.exception}")
+                }
+            }
+
+
+    }
+
+
+
+    fun showToast(str: String){
+        Toast.makeText(applicationContext, str, Toast.LENGTH_SHORT).show()
+    }
+    fun showToast(strId: Int){
+        Toast.makeText(applicationContext, strId, Toast.LENGTH_SHORT).show()
     }
 }
