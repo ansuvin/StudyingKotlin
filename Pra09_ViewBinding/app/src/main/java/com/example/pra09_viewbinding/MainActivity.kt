9edb27037a4caee608f976667cf2219c7e363569
ansuvin <dkstnqls0925@naver.com>
2021-02-04 15:16:07 +0900

    [UPDATE] ViewBinding

--- Pra09_ViewBinding/app/src/main/java/com/example/pra09_viewbinding/MainActivity.kt
+++ Pra09_ViewBinding/app/src/main/java/com/example/pra09_viewbinding/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.pra09_viewbinding
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.pra09_viewbinding.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+
+        binding.title.text = "Hello"
+        binding.subtitle.text = "Concise, safe code"
+        binding.button.setOnClickListener {
+            binding.title.text = "버튼"
+            binding.subtitle.text = "눌림"
+        }
+
+        setContentView(binding.root)
     }
 }
