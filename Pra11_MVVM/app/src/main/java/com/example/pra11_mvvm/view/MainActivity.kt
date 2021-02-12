f8158eb15665302020bfd1a59bd48c4404279819
ansuvin <dkstnqls0925@naver.com>
2021-02-12 11:29:09 +0900

    [UPDATE] CounterViewModel

--- Pra11_MVVM/app/src/main/java/com/example/pra11_mvvm/view/MainActivity.kt
+++ Pra11_MVVM/app/src/main/java/com/example/pra11_mvvm/view/MainActivity.kt
@@ -1,17 +1,38 @@
 package com.example.pra11_mvvm.view
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProviders
+import com.example.pra11_mvvm.R
+import com.example.pra11_mvvm.databinding.ActivityMainBinding
+import com.example.pra11_mvvm.viewmodel.CounterViewModel
 import com.example.pra11_mvvm.viewmodel.MainViewModel
 
 class MainActivity:AppCompatActivity() {
-    private lateinit var viewModel: MainViewModel
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var viewModel: CounterViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        // ViewModel 인스턴스 생성
-        viewModel = ViewModelProvider(this,
-        ViewModelProvider.NewInstanceFactory()).get(MainViewModel::class.java)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.viewModel = this
+
+        // viewModel 가져오기
+        viewModel = ViewModelProvider(this, ViewModelProvider.NewInstanceFactory())
+            .get(CounterViewModel::class.java)
     }
+
+    fun onAddButtonClick(view: View) {
+        viewModel.counter++
+        binding.counterTV.text = "${viewModel.counter} "
+    }
+
+    fun onSubButtonClick(view: View) {
+        viewModel.counter--
+        binding.counterTV.text = "${viewModel.counter} "
+    }
+
 }
---
+++ Pra11_MVVM/app/src/main/java/com/example/pra11_mvvm/viewmodel/CounterViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.pra11_mvvm.viewmodel
+
+import androidx.lifecycle.ViewModel
+
+class CounterViewModel:ViewModel() {
+    public var counter =  0
+}
